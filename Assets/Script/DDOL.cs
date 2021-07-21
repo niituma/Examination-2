802df2c1f49d61df8c213203d4756b14f48e3983
sakezuma <sakezuma@gmail.com>
2021-07-21 20:28:41 +0900

    scene　追加 camera　変更

---
+++ Assets/Script/DDOL.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDOL : MonoBehaviour
+{
+        void Start()
+        {
+            if (FindObjectsOfType<DDOL>().Length > 1)
+            {
+                // 重複しないように、既にある時は自分自身を破棄する
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                // 自分しかいない時は、自分を DontDestroyOnLoad に登録する
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+
+    }
