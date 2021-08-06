5cd3a40587e8c5eab2cae5b206016c1528512e7c
sakezuma <sakezuma@gmail.com>
2021-08-06 18:15:36 +0900

    攻撃反応のテストプログラムを作った

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private string AttackTag = "Air";
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == AttackTag)
+        {
+            Debug.Log("攻撃を受けた");
+        }
+    }
+}
