d796feacda31e3cc9d8e074004eb7e1bd440da24
sakezuma2 <sakezuma@gmail.com>
2021-08-19 03:13:36 +0900

    rabbitにプレイヤー追跡を追加した

--- Assets/Script/RabbitController.cs
+++ Assets/Script/RabbitController.cs
@@ -5,26 +5,23 @@ using UnityEngine;
 public class RabbitController : MonoBehaviour
 {
     int Hitpoint;
-    [SerializeField] float m_Speed = 3;
     private string AttackTag = "Attackpoint";
-    [SerializeField] GameObject Player = default;
     Animator m_anim = default;
-    Rigidbody2D m_rb = default;
 
     private void Start()
     {
         m_anim = GetComponent<Animator>();
-        m_rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        if(Hitpoint==3)
+        if (Hitpoint == 3)
         {
+            Destroy(this.GetComponent<Rabbitmove>());
             m_anim.SetBool("Dead", true);
         }
-        this.transform.position = Vector2.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y), m_Speed * Time.deltaTime);
 
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == AttackTag)
@@ -33,15 +30,24 @@ public class RabbitController : MonoBehaviour
             Debug.Log("攻撃を受けた");
             m_anim.SetBool("Hit", true);
         }
-        
+        if (collision.tag == "camera")
+        {
+            m_anim.SetBool("Walk", true);
+        }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         m_anim.SetBool("Hit", false);
+        if (collision.tag == "camera")
+        {
+            m_anim.SetBool("Walk", false);
+        }
     }
     void Destroy()
     {
         Destroy(this.gameObject);
     }
+    
 }
 
---
+++ Assets/Script/Rabbitmove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbitmove : MonoBehaviour
+{
+    [SerializeField] float m_Speed = 3f;
+    [SerializeField] GameObject Player = default;
+    [SerializeField] GameObject REnemy = default;
+    Rigidbody2D m_rb = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "camera")
+        {
+        REnemy.transform.position = Vector2.MoveTowards(this.transform.position,new Vector2(Player.transform.position.x,transform.position.y),m_Speed * Time.deltaTime);
+        }
+    }
+}
