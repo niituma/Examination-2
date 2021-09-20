a3b7f1b79c0c734f91563fc78a58672b7a4b5574
sakezuma2 <sakezuma@gmail.com>
2021-09-21 03:19:51 +0900

    ボス攻撃　パターン作成中

--- Assets/Script/Enemy/BossController.cs
+++ Assets/Script/Enemy/BossController.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    [SerializeField] float m_attacktime = 5f;
+    [SerializeField] float m_attack1time = 5f;
+    [SerializeField] float m_attack23time = 20f;
     [SerializeField] float m_dis = 0;
     [SerializeField] float m_attackareadis = 5f;
+    [SerializeField] float m_attack1cooltime = 0;
+    [SerializeField] float m_attack23cooltime = 0;
+    [SerializeField] float attack3movespeed = 0.5f;
+    [SerializeField] float attack2movespeed = 5f;
+    [SerializeField] GameObject m_laser = default;
+    GameObject Attack2pos = default;
     GameObject Player = default;
-    float m_attack1cooltime = 0;
+    bool Skill = default;
+    bool ismoving2 = default;
+    bool ismoving3 = default;
+    Vector2 pos;
     Animator m_anim = default;
     Rigidbody2D m_rb = default;
 
@@ -18,6 +28,8 @@ public class BossController : MonoBehaviour
         m_anim = GetComponent<Animator>();
         m_rb = GetComponent<Rigidbody2D>();
         Player = GameObject.Find("Player");
+        Attack2pos = GameObject.Find("Attack2Pos");
+        pos = this.transform.position;
     }
 
     // Update is called once per frame
@@ -26,7 +38,20 @@ public class BossController : MonoBehaviour
         Vector2 PoseA = (Vector2)(Player?.transform.position);
         Vector3 PoseB = this.transform.position;
         m_dis = Vector2.Distance(PoseA, PoseB);
+
+
+        if (!Skill)
+            m_attack23cooltime += Time.deltaTime;
+
+        if (ismoving3)
+            this.gameObject.transform.position = new Vector2(this.transform.position.x - attack3movespeed, this.transform.position.y);
+
+        if (ismoving2)
+            this.transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(Attack2pos.transform.position.x, Attack2pos.transform.position.y + 3), attack2movespeed * Time.deltaTime);
+
+
         Attack1();
+        Attack23();
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -38,7 +63,7 @@ public class BossController : MonoBehaviour
 
     void Attack1()
     {
-        if (m_dis <= m_attackareadis && m_attack1cooltime >= m_attacktime)
+        if (m_dis <= m_attackareadis && m_attack1cooltime >= m_attack1time)
         {
             m_anim.SetBool("Attack1", true);
             m_attack1cooltime = 0f;
@@ -47,5 +72,51 @@ public class BossController : MonoBehaviour
         {
             m_anim.SetBool("Attack1", false);
         }
+    }
+    void Attack23()
+    {
+        if (m_attack23cooltime >= m_attack23time)
+        {
+            Skill = true;
+            m_attack23cooltime = 0f;
+            int Attack = Random.Range(1, 3);
+            if (Attack == 1)
+            {
+                ismoving2 = true;
+                m_anim.SetBool("Attack2", true);
+            }
+            else
+            {
+                m_anim.SetBool("Attack3", true);
+                transform.Rotate(new Vector3(0, 0, 90));
+                ismoving3 = true;
+            }
+        }
+        else
+        {
+            m_anim.SetBool("Attack2", false);
+            m_anim.SetBool("Attack3", false);
+        }
+    }
+    void Skillfalse()
+    {
+        Skill = false;
+    }
+    void AttackMovefalse()
+    {
+        ismoving2 = false;
+        ismoving3 = false;
+    }
+    void Attack3Rerotate()
+    {
+        transform.Rotate(new Vector3(0, 0, -90));
+    }
+    void LaserOn()
+    {
+        m_laser.SetActive(true);
+    }
+    void LaserOff()
+    {
+        m_laser.SetActive(false);
     }
 }
