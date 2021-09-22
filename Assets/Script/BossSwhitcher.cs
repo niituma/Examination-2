bcf6acd64aaedad2c2aab70702136cb2c657e18e
sakezuma2 <sakezuma@gmail.com>
2021-09-22 18:59:28 +0900

    Player Timeline 実装

---
+++ Assets/Script/BossSwhitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSwhitcher : MonoBehaviour
+{
+    GameObject m_boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_boss = GameObject.FindGameObjectWithTag("Boss");
+    }
+    public void TurnOff()
+    {
+        if (m_boss)
+            m_boss.SetActive(false);
+    }
+    public void TurnOn()
+    {
+        if (m_boss)
+            m_boss.SetActive(true);
+    }
+}
---
+++ Assets/Script/PlayerSwhitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwhitcher : MonoBehaviour
+{
+    GameObject m_player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+    }
+    public void TurnOff()
+    {
+        if (m_player)
+            m_player.SetActive(false);
+    }
+    public void TurnOn()
+    {
+        if (m_player)
+            m_player.SetActive(true);
+    }
+}
