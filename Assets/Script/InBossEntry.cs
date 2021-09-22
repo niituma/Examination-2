c4cba53f710d8f9f77b43ee162d8136d1db89be5
sakezuma2 <sakezuma@gmail.com>
2021-09-23 00:09:32 +0900

    timeline boss

---
+++ Assets/Script/InBossEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InBossEntry : MonoBehaviour
+{
+    [SerializeField] GameObject m_woodwall;
+    [SerializeField] GameObject m_bossentey;
+    private void Start()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        m_woodwall.SetActive(true);
+        m_bossentey.SetActive(true);
+        Destroy(this.gameObject);
+    }
+}
