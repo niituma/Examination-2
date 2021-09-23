ca429902e9af845225dd0bdbf7ea8d28ad5d5513
sakezuma <sakezuma@gmail.com>
2021-09-24 05:00:55 +0900

    修正

---
+++ Assets/Script/ActiveHp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveHp : MonoBehaviour
+{
+    GameObject HP;
+    float m_heel = 155f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = GameObject.Find("SliderHP");
+        HP.SetActive(true);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            FindObjectOfType<PlayerHP>().AddLife(m_heel);
+            Destroy(this.gameObject);
+        }
+    }
+}
