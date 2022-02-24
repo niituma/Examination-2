432c25503b9a238eb4268e5e7564ebea8a5f9526
sakezuma <sakezuma@gmail.com>
2022-02-24 19:58:46 +0900

    test

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //
     [SerializeField] GameObject PausePanel = default;
     bool ispause = default;
     private void Update()
